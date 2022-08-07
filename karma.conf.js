c91c42d8af213606b7d48355e47d95e17b6bb84f
dzmitryklimenak <dk@flexistax.com>
2022-08-08 02:10:19 +0300

    update project name

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/account-test'),
+      dir: require('path').join(__dirname, './coverage/account-list'),
       subdir: '.',
       reporters: [
         { type: 'html' },
